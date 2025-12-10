03188ee0be3e00b70a40a846b38c0e275ab6b04d
Nimit Jalan <nimitjalan2013@gmail.com>
2025-12-10 09:19:43 +0400

    fix Oauth

--- backend/site_settings/urls.py
+++ backend/site_settings/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
+    path("accounts/", include("allauth.urls")), # Required for allauth to resolve named URLs
 ]
